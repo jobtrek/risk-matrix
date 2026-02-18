6b225dede8e14bc9b0f65bd18041c37a76e27c5c
adbme <adil.bouamrioune@gmail.com>
2026-02-18 08:42:59 +0100

    feat: add environment configuration files and update useMatrix hook to use dynamic server URL

--- apps/web/src/hooks/useMatrix.ts
+++ apps/web/src/hooks/useMatrix.ts
@@ -2,13 +2,19 @@ import { treaty } from "@elysiajs/eden";
 
 import type { App } from "../../../server/src/index";
 
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
+if (!serverUrl) {
+  throw new Error("VITE_SERVER_URL not found");
+}
+
 // treaty transforme api en objet js typé (a la place de fetch/axios)
 // on peut (depuis api.) accéder à /matrix, /auth car lit le type App du serveur
-const client = treaty<App>("http://localhost:3000", {
-    fetch: {
-        // force envoie de cookies (stocké dans navigateur pour envoyer au serveur)
-        credentials: "include",
-    }
+const client = treaty<App>(serverUrl, {
+  fetch: {
+    // force envoie de cookies (stocké dans navigateur pour envoyer au serveur)
+    credentials: "include",
+  },
 });
 
 export const api = client;
